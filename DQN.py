96ce1b4925e603ab15bf18a059a59abf881716a6
julianalverio <julian.a.alverio@gmail.com>
2019-01-09 13:52:14 -0500

    initial commit

---
+++ DQN.py
@@ -0,0 +1,156 @@
+import torch
+import torch.nn as nn
+import random
+import numpy as np
+from torch.autograd import Variable
+from collections import namedtuple
+import copy
+import torch.optim as optim
+
+
+HYPERPARAMS = {
+        'replay_size':      100000,
+        'replay_initial':   10000,
+        'target_net_sync':  1000,
+        'epsilon_frames':   10**5,
+        'epsilon_start':    1.0,
+        'epsilon_final':    0.02,
+        'learning_rate':    0.0001,
+        'gamma':            0.99,
+        'batch_size':       32
+}
+
+TRANSITION = namedtuple('Transition', ('state', 'action', 'reward', 'next_state'))
+
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+        self.transition = TRANSITION
+
+    def push(self, *args):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = self.transition(*args)
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+
+
+class EpsilonTracker:
+    def __init__(self):
+        self._epsilon = HYPERPARAMS['epsilon_start']
+        self.epsilon_final = HYPERPARAMS['epsilon_final']
+        self.epsilon_delta = 1.0 * (HYPERPARAMS['epsilon_start'] - HYPERPARAMS['epsilon_final']) / HYPERPARAMS['epsilon_frames']
+
+    def epsilon(self):
+        old_epsilon = self._epsilon
+        self._epsilon -= self.epsilon_delta
+        return max(old_epsilon, self.epsilon_final)
+
+
+
+class DQN(nn.Module):
+    def __init__(self, input_shape, num_actions, device):
+        super(DQN, self).__init__()
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out = self.conv(Variable(torch.zeros(1, *input_shape)))
+        conv_out_size = int(np.prod(conv_out.size()))
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, num_actions)
+        )
+        self.num_actions = num_actions
+        self.counter = 0
+        self.device = device
+        self.memory = ReplayMemory(HYPERPARAMS['replay_size'])
+        self.epsilon_tracker = EpsilonTracker()
+        self.optimizer = optim.Adam(self.parameters(), lr=HYPERPARAMS['learning_rate'])
+
+
+    def forward(self, x):
+        x = self.conv(x).view(x.size()[0], -1)
+        return self.fc(x)
+
+
+    def getAction(self, state):
+        if random.random() < self.epsilon_tracker.epsilon():
+            return torch.tensor([random.randrange(self.num_actions)], device=self.device)
+        else:
+            return torch.argmax(self(state), dim=1).to(self.device)
+
+    def optimizeModel(self, target_net):
+        self.counter += 1
+        transitions = self.memory.sample(HYPERPARAMS['batch_size'])
+        batch = TRANSITION(*zip(*transitions))
+        non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)), device=self.device, dtype=torch.uint8)
+        non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
+        state_batch = torch.cat(list(batch.state))
+        action_batch = torch.cat(list(batch.action))
+        reward_batch = torch.cat(list(batch.reward))
+        state_action_values = self(state_batch).gather(1, action_batch.unsqueeze(1))
+        next_state_values = torch.zeros(HYPERPARAMS['batch_size'], device=self.device)
+        next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+        expected_state_action_values = (next_state_values * HYPERPARAMS['gamma']) + reward_batch
+        loss = nn.MSELoss()(state_action_values, expected_state_action_values.unsqueeze(1))
+        self.optimizer.zero_grad()
+        loss.backward()
+        # for param in self.parameters():
+        #     param.grad.data.clamp_(-1, 1)
+        self.optimizer.step()
+        if self.counter % HYPERPARAMS['target_net_sync'] == 0:
+            target_net.load_state_dict(self.state_dict())
+
+
+
+class Dueling_DQN(nn.Module):
+    def __init__(self, input_shape, num_actions):
+        super(Dueling_DQN, self).__init__()
+        self.num_actions = num_actions
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out = self.conv(Variable(torch.zeros(1, *input_shape)))
+        conv_out_size = int(np.prod(conv_out.size()))
+        self.fc_adv = nn.Sequential(
+            nn.Linear(in_features=conv_out_size, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=num_actions)
+        )
+
+        self.fc_val = nn.Sequential(
+            nn.Linear(in_features=conv_out_size, out_features=512),
+            nn.ReLU(),
+            nn.Linear(in_features=512, out_features=1)
+        )
+
+    def forward(self, x):
+        x = self.conv(x).view(x.size()[0], -1)
+        adv = self.fc_adv(x)
+        val = self.fc_val(x).expand(x.size(0), self.num_actions)
+        return val + adv - adv.mean(1).unsqueeze(1).expand(x.size(0), self.num_actions)
+
+
---
+++ pinball/pinball.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+import torch
+torch.backends.cudnn.deterministic = True
+torch.manual_seed(5)
+torch.cuda.manual_seed_all(5)
+import random
+random.seed(5)
+import numpy as np
+np.random.seed(5)
+
+import gym
+import torch
+import csv
+
+from tensorboardX import SummaryWriter
+
+import torch.nn as nn
+import copy
+from collections import namedtuple
+from torch.autograd import Variable
+from DQN import *
+from wrappers import wrap_dqn
+
+
+import os; os.environ["CUDA_VISIBLE_DEVICES"]="1"
+from DQN import HYPERPARAMS
+
+
+class RewardTracker:
+    def __init__(self, length=100, stop_reward=20):
+        self.stop_reward = stop_reward
+        self.length = length
+        self.rewards = []
+        self.position = 0
+        self.stop_reward = stop_reward
+        self.mean_score = 0
+
+    def add(self, reward):
+        if len(self.rewards) < self.length:
+            self.rewards.append(reward)
+        else:
+            self.rewards[self.position] = reward
+            self.position = (self.position + 1) % self.length
+        self.mean_score = np.mean(self.rewards)
+
+    def meanScore(self):
+        return self.mean_score
+
+
+class Trainer(object):
+    def __init__(self):
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.env = gym.make('VideoPinballNoFrameskip-v4')
+        self.env = wrap_dqn(self.env)
+
+        self.policy_net = DQN(self.env.observation_space.shape, self.env.action_space.n, self.device).to(self.device)
+        self.target_net = copy.deepcopy(self.policy_net)
+        self.reward_tracker = RewardTracker()
+        self.episode = 0
+        self.state = self.preprocess(self.env.reset())
+        self.score = 0
+        self.batch_size = HYPERPARAMS['batch_size']
+
+        csv_file = open('vanilla_dqn.csv', 'w+')
+        self.writer = csv.writer(csv_file)
+
+
+    def preprocess(self, state):
+        state = torch.tensor(np.expand_dims(state, 0)).to(self.device)
+        return state.float() / 256
+
+
+    def addExperience(self):
+        action = self.policy_net.getAction(self.state)
+        next_state, reward, done, _ = self.env.step(action.item())
+        self.score += reward
+        if done:
+            self.policy_net.memory.push(self.state, action, torch.tensor([reward], device=self.device), None)
+            self.state = self.preprocess(self.env.reset())
+            self.episode += 1
+        else:
+            next_state = self.preprocess(next_state)
+            self.policy_net.memory.push(self.state, action, torch.tensor([reward], device=self.device), next_state)
+            self.state = next_state
+        return done
+
+
+    def train(self):
+        while True:
+            game_over = self.addExperience()
+            if game_over:
+                self.reward_tracker.add(self.score)
+                print('Game: %s Score: %s Mean Score: %s' % (self.episode, self.score, self.reward_tracker.meanScore()))
+                self.score = 0
+
+            # are we done prefetching?
+            if len(self.policy_net.memory) < HYPERPARAMS['replay_initial']:
+                continue
+
+            self.policy_net.optimizeModel(self.target_net)
+            self.writer.writerow([self.episode, self.score, self.reward_tracker.meanScore(), self.policy_net.epsilon_tracker._epsilon])
+
+
+
+    # def playback(self, path):
+    #     target_net = torch.load(path, map_location='cpu')
+    #     env = gym.make('PongNoFrameskip-v4')
+    #     env = wrap_dqn(env)
+    #     state = self.preprocess(env.reset())
+    #     done = False
+    #     score = 0
+    #     import time
+    #     while not done:
+    #         time.sleep(0.015)
+    #         env.render(mode='human')
+    #         action = torch.argmax(target_net(state), dim=1).to(self.device)
+    #         state, reward, done, _ = env.step(action.item())
+    #         state = self.preprocess(state)
+    #         score += reward
+    #     print("Score: ", score)
+
+
+
+if __name__ == "__main__":
+    trainer = Trainer()
+    print('Trainer Initialized')
+    trainer.train()
+
+
+
+
+
+
