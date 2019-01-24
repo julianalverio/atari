875e8c38925b7da64176a5d0c96a9c04bf1a997e
julianalverio <julian.a.alverio@gmail.com>
2019-01-24 09:28:07 -0800

    iterating

--- pinball/pinball.py
+++ pinball/pinball.py
@@ -73,7 +73,7 @@ class Trainer(object):
     def train(self):
         total_steps = 0
         for episode in range(NUM_EPISODES):
-            for iteration in range(1e8)
+            for iteration in range(1e8):
                 total_steps += 1
                 game_over = self.addExperience()
 
