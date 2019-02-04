229eb1213d7486b406739f7575e7a5eb88711b61
julianalverio <julian.a.alverio@gmail.com>
2019-02-04 09:52:56 -0500

    iterating

--- seaquest/prioritized_buffer.py
+++ seaquest/prioritized_buffer.py
@@ -181,7 +181,10 @@ class PrioritizedReplayBuffer(ReplayBuffer):
         """
         assert len(idxes) == len(priorities)
         for idx, priority in zip(idxes, priorities):
-            assert priority > 0
+            try:
+                assert priority > 0
+            except:
+                import pdb; pdb.set_trace()
             assert 0 <= idx < len(self._storage)
             self._it_sum[idx] = priority ** self._alpha
             self._it_min[idx] = priority ** self._alpha
