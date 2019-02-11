5e0929f60d311e8c8d322ca92401403c8552a20e
julianalverio <julian.a.alverio@gmail.com>
2019-02-11 13:23:16 -0500

    iterating

--- seaquest/prioritized/seaquest_prioritized.py
+++ seaquest/prioritized/seaquest_prioritized.py
@@ -41,15 +41,15 @@ from queues import PrioritizedReplayBuffer as Memory
 
 
 HYPERPARAMS = {
-        'replay_size':      100000,  # 10K
-        'replay_initial':   100000,
+        'replay_size':      25000,  # 25K
+        'replay_initial':   25000,
         'target_net_sync':  1000,
         'epsilon_frames':   10**5,
         'epsilon_start':    1.0,
         'epsilon_final':    0.02,
         'learning_rate':    0.0001,
         'gamma':            0.99,
-        'batch_size':       32
+        'batch_size':       64
 }
 
 
